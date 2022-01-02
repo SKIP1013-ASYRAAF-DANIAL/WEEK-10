8f717cd318bfd4a512d37104bb48cec206b507e8
AHMAD ASYRAAF DANIAL BIN NORAZLAN 284368 <92504346+AsyraafDanial-284368@users.noreply.github.com>
2022-01-03 00:32:40 +0800

    Create CODING EXE3.java

---
+++ CODING EXE3.java
@@ -0,0 +1,22 @@
+package selfProgramming;
+
+public class EXE3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Write a Java program to print the following grid
+		
+		int[][] grid = new int [5][5];
+				
+		for (int i = 0; i < 5; i++) 
+		{
+			for (int j = 0; j < 5; j++) 
+			{
+				System.out.printf("%2d ", grid[i][j]);
+			}
+			System.out.println();
+	}		
+
+	}
+
+}
